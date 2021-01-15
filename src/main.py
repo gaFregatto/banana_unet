792cfb040a98ee6c32f8b851a04b02d222e3e3dd
Guilherme Arai Fregatto <guilherme_arai19@hotmail.com>
2021-01-15 19:50:35 -0300

    forced train augmentation && added model with kernel regularizers

--- src/main.py
+++ src/main.py
@@ -3,7 +3,16 @@ from utils import *
 
 def main():
     # Data augmentation
-    train_gen_args = dict(rotation_range=30,
+    train_gen_args = dict(rotation_range=40,
+                    width_shift_range=0.45,
+                    height_shift_range=0.45,
+                    shear_range=0.2,
+                    zoom_range=0.2,
+                    horizontal_flip=True,
+                    vertical_flip=True,
+                    fill_mode='nearest')
+
+    val_gen_args = dict(rotation_range=20,
                     width_shift_range=0.05,
                     height_shift_range=0.05,
                     shear_range=0.05,
@@ -12,46 +21,119 @@ def main():
                     vertical_flip=True,
                     fill_mode='nearest')
 
-    val_gen_args = dict(rotation_range=40,
-                    width_shift_range=0.05,
-                    height_shift_range=0.05,
-                    shear_range=0.05,
-                    zoom_range=0.05,
-                    horizontal_flip=True,
-                    vertical_flip=True,
-                    fill_mode='nearest')
-
-    trainGene = data_augmentation(4, '../dataset/train/', 'img', 'mask', 
+    trainGene = data_augmentation(BATCH_TRAIN, '../dataset/train/', 'img', 'mask', 
                             train_gen_args, save_to_dir=None, save_format=None)
     
-    valGene = data_augmentation(4, '../dataset/val/', 'img', 'mask', 
+    valGene = data_augmentation(BATCH_VAL, '../dataset/val/', 'img', 'mask', 
                             val_gen_args, save_to_dir=None, save_format=None)
 
     # U-net architecture
-    # model = unet_paper((IMG_WIDTH, IMG_HEIGHT, IMG_CHANNELS))
+    # model = unet_reg((IMG_WIDTH, IMG_HEIGHT, IMG_CHANNELS))
     model = unet_test((IMG_WIDTH, IMG_HEIGHT, IMG_CHANNELS))
     
-    # Modelcheckpoint
+    # Modelcheckpoints
     checkpointer = ModelCheckpoint(os.path.join('../checkpoints', FILE), verbose=1, save_best_only=True)
-    callbacks = [EarlyStopping(patience=8, monitor='val_loss'),
-                TensorBoard(log_dir='../logs'),
-                checkpointer]
+    csv_logger = CSVLogger(os.path.join('../csv_logs', FILE_CSV), separator=',', append=False)
+    early_stop = EarlyStopping(patience=5, monitor='val_loss')
+    tensor_board = TensorBoard(log_dir='../logs')
+    callbacks = [checkpointer, csv_logger, tensor_board, early_stop]
     
     # Training
-    model.fit(trainGene, epochs=EP,
+    history = model.fit(trainGene, epochs=EP,
                          steps_per_epoch=STEP,
                          validation_data=valGene,
-                         validation_steps=VSTEP, 
+                         validation_steps=V_STEP, 
                          callbacks=callbacks)
 
     # Save model 
     model.save(os.path.join('../saved_models', FILE))
-
     
-if __name__ == '__main__':
-    # resize_images('../dataset', IMG_WIDTH, IMG_HEIGHT)
-    # generate_masks()
-    main()
+    # Evaluating 
+    print("Training evaluate")
+    train_loss, train_acc = model.evaluate(trainGene, steps=E_STEP)
+    print(f"Train accuracy - loss: {train_acc} - {train_loss}")
+    
+    print("Validation evaluate")
+    val_loss, val_acc = model.evaluate(valGene, steps=E_STEP)
+    print(f"Validation accuracy - loss: {val_acc} - {val_loss}")
 
+    # Predicting results
+    testGene = testGenerator('../dataset/test', gray_flag=True)
+    results = model.predict(testGene, 40, verbose=1)
+    saveResult("../dataset/test",results)
+
+    # Plotting
+    plt.plot(history.history['loss'], label='train_loss')
+    plt.plot(history.history['val_loss'], label='val_loss')
+    plt.legend()
+    plt.savefig(os.path.join('../plots', FILE_PNG))
+    # plt.show()
+
+
+def retrain():
+    train_gen_args = dict(rotation_range=40,
+                    width_shift_range=0.45,
+                    height_shift_range=0.45,
+                    shear_range=0.2,
+                    zoom_range=0.2,
+                    horizontal_flip=True,
+                    vertical_flip=True,
+                    fill_mode='nearest')
+
+    val_gen_args = dict(rotation_range=20,
+                    width_shift_range=0.05,
+                    height_shift_range=0.05,
+                    shear_range=0.05,
+                    zoom_range=0.05,
+                    horizontal_flip=True,
+                    vertical_flip=True,
+                    fill_mode='nearest')
+
+    trainGene = data_augmentation(BATCH_TRAIN, '../dataset/train/', 'img', 'mask', 
+                            train_gen_args, save_to_dir=None, save_format=None)
+    
+    valGene = data_augmentation(BATCH_VAL, '../dataset/val/', 'img', 'mask', 
+                            val_gen_args, save_to_dir=None, save_format=None)
+    
+    model = load_model('../saved_models/test_256x256x1_ep25_step400_vstep150.h5')
+    
+    results = model.evaluate(valGene, steps=E_STEP)
+    print("validation loss and accuracy are", results)
+
+    checkpointer = ModelCheckpoint(os.path.join('../checkpoints', FILE), verbose=1, save_best_only=True)
+    csv_logger = CSVLogger(os.path.join('../csv_logs', FILE_CSV), separator=',', append=True)
+    early_stop = EarlyStopping(patience=5, monitor='val_loss')
+    tensor_board = TensorBoard(log_dir='../logs')
+    callbacks = [checkpointer, csv_logger, tensor_board, early_stop]
+
+    history = model.fit(trainGene, epochs=EP,
+                         steps_per_epoch=STEP,
+                         validation_data=valGene,
+                         validation_steps=V_STEP, 
+                         callbacks=callbacks)
+
+    model.save(os.path.join('../retrained_models', FILE))
+
+
+def gen_results():
+    model = load_model('../checkpoints/F_test_256x256x1_ep30_step300_vstep60_bt8_bv2.h5')
+    # model = load_model('../saved_models/F_test_256x256x1_ep30_step300_vstep60_bt8_bv2.h5')
+    
+    testGene = testGenerator('../dataset/test', gray_flag=True)
+    results = model.predict(testGene, 40, verbose=1)
+    saveResult("../dataset/test",results)
+    
+    # set_trace()
+
+
+if __name__ == '__main__':
+    # Para treinar um novo modelo
+    main()
+    
+    # Para continuar treinando um modelo salvo
+    # retrain()
+
+    # Para gerar resultados a partir de um modelo salvo
+    # gen_results()
     
 
--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,29 @@
 from keras.preprocessing.image import ImageDataGenerator
+from keras.models import load_model
 import matplotlib.pyplot as plt 
+from pdb import set_trace
 import numpy as np 
 import cv2 as cv 
 import json 
 import os
 
+MODEL = 'F_test'
+
 IMG_HEIGHT = 256
 IMG_WIDTH = 256
 IMG_CHANNELS = 1
 
-EP = 100
-STEP = 2000 
-VSTEP = 1000
+BATCH_TRAIN = 8
+BATCH_VAL = 2
 
-FILE = f"test_{IMG_WIDTH}x{IMG_HEIGHT}x{IMG_CHANNELS}_ep{EP}_step{STEP}_vstep{VSTEP}.h5"
+EP = 30
+STEP = 300
+V_STEP = 60
+E_STEP = 150
+
+FILE = f"{MODEL}_{IMG_WIDTH}x{IMG_HEIGHT}x{IMG_CHANNELS}_ep{EP}_step{STEP}_vstep{V_STEP}_bt{BATCH_TRAIN}_bv{BATCH_VAL}.h5"
+FILE_CSV = f"{MODEL}_{IMG_WIDTH}x{IMG_HEIGHT}x{IMG_CHANNELS}_ep{EP}_step{STEP}_vstep{V_STEP}_bt{BATCH_TRAIN}_bv{BATCH_VAL}.csv"
+FILE_PNG = f"{MODEL}_{IMG_WIDTH}x{IMG_HEIGHT}x{IMG_CHANNELS}_ep{EP}_step{STEP}_vstep{V_STEP}_bt{BATCH_TRAIN}_bv{BATCH_VAL}.png"
 
 COLORS = [[43, 43, 200], [43, 75, 200], [43, 106, 200], [43, 137, 200], [43, 169, 200], [43, 200, 195], [43, 200, 163], 
             [43, 200, 132], [43, 200, 101], [43, 200, 69], [54, 200, 43], [85, 200, 43], [116, 200, 43], [148, 200, 43], 
@@ -91,22 +101,19 @@ def plot_pair(images, gray=False):
     plt.show()
 
 
-def adjustData(img,mask,flag_multi_class,num_class):
-    img = img / 255
-    mask = mask /255
-    mask[mask > 0.5] = 1
-    mask[mask <= 0.5] = 0
+def adjustData(img,mask):
+    if(np.max(img) > 1):
+        img = img/255
+        mask = mask/255
+        mask[mask > 0.5] = 1
+        mask[mask <= 0.5] = 0
     return (img,mask)
 
 
 def data_augmentation(batch_size, folder_path, image_folder, mask_folder, aug_dict, image_color_mode ="grayscale",
                     mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask",
-                    flag_multi_class=False, num_class=1, save_to_dir=None, save_format=None,  target_size=(IMG_WIDTH,IMG_HEIGHT), seed=1):
-    '''
-    can generate image and mask at the same time
-    use the same seed for image_datagen and mask_datagen to ensure the transformation for image and mask is the same
-    if you want to visualize the results of generator, set save_to_dir = "your path"
-    '''
+                    save_to_dir=None, save_format=None,  target_size=(IMG_WIDTH,IMG_HEIGHT), seed=1):
+   
     image_datagen = ImageDataGenerator(**aug_dict)
     mask_datagen = ImageDataGenerator(**aug_dict)
     image_generator = image_datagen.flow_from_directory(
@@ -133,5 +140,28 @@ def data_augmentation(batch_size, folder_path, image_folder, mask_folder, aug_di
         seed = seed)
     train_generator = zip(image_generator, mask_generator)
     for (img, mask) in train_generator:
-        img, mask = adjustData(img, mask, flag_multi_class, num_class)
+        img, mask = adjustData(img, mask)
         yield (img, mask)
+
+def testGenerator(test_path, num_image=40, target_size=(IMG_WIDTH,IMG_HEIGHT), gray_flag=True):
+
+    for i in range(num_image):
+        if gray_flag == False:
+            img = cv.imread(os.path.join(test_path,"%d.jpg"%i))
+            img = img/255
+            img = np.array(cv.resize(img, (IMG_WIDTH, IMG_HEIGHT)))
+            img = np.reshape(img,(1,)+img.shape)
+            yield img
+        elif gray_flag == True:
+            img = cv.imread(os.path.join(test_path,"%d.jpg"%i), 0)
+            img = img/255
+            img = np.array(cv.resize(img, (IMG_WIDTH, IMG_HEIGHT)))
+            img = np.reshape(img,img.shape+(1,)) if (gray_flag) else img
+            img = np.reshape(img,(1,)+img.shape)
+            yield img
+
+
+def saveResult(save_path, data):
+    for i, img in enumerate(data): 
+        img = img * 255
+        cv.imwrite(os.path.join(save_path, "%d_predict.jpg"%i), img)
